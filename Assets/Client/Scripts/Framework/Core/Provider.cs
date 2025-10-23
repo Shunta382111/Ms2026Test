bf2acfbd43d4d349224312663f091400e02e43da
ao_nea_one <ahamayama13@gmail.com>
2025-10-23 12:38:58 +0900

    プロバイダの基底クラスを作成

---
+++ Assets/Client/Scripts/Framework/Core/Provider.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+namespace Framework.Core
+{
+    public class Provider
+    {
+        protected T GetOrAddComponent<T>(GameObject owner) where T : Component
+        {
+            DebugEx.AssertSome(owner, "ゲームオブジェクトが null です");
+            T component = null;
+
+            // コンポーネントを取得
+            if (!owner.TryGetComponent(out component))
+            {
+                // 失敗 → コンポーネントを追加
+                DebugEx.Log($"{owner.name} にコンポーネントを追加します: {typeof(T).Name}");
+                component = owner.AddComponent<T>();
+            }
+
+            return component;
+        }
+    }
+}
