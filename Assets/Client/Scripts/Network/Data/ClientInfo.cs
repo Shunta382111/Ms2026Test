dd9e9fa78f9dbd4eb2bbbd80db2425a60b5ae24f
ao_nea_one <ahamayama13@gmail.com>
2025-10-23 11:47:35 +0900

    コンパイルエラーの無い状態に仮修正

--- Assets/Client/Scripts/Network/Data/ClientInfo.cs
+++ Assets/Client/Scripts/Network/Data/ClientInfo.cs
@@ -4,10 +4,20 @@ using Unity.Netcode;
 
 namespace Network
 {
-    public class ClientID
+    [Serializable]
+    public class ClientID : IEquatable<ClientID>
     {
-        public ulong id = 0;
+        public ulong Value = 0;
+        public static readonly ClientID Empty = new ClientID(0);
+        public ClientID(ulong id) { this.Value = id; }
+
+        /// <summary>
+        /// 等比較
+        /// </summary>
+        public bool Equals(ClientID other) => Value == other.Value;
     }
+
+
 
     [Serializable]
     public class ClientInfo : INetworkSerializable, IEquatable<ClientInfo>
@@ -18,17 +28,17 @@ namespace Network
             Ready
         }
 
-        public ulong id = 0;
-        public FixedString64Bytes name = new FixedString64Bytes();
-        public ConnectionState state = ConnectionState.Wait;
+        public ClientID Id = ClientID.Empty;
+        public FixedString64Bytes Name = new FixedString64Bytes();
+        public ConnectionState State = ConnectionState.Wait;
 
         public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
         {
-            serializer.SerializeValue(ref id);
-            serializer.SerializeValue(ref name);
-            serializer.SerializeValue(ref state);
+            serializer.SerializeValue(ref Id.Value);
+            serializer.SerializeValue(ref Name);
+            serializer.SerializeValue(ref State);
         }
 
-        public bool Equals(ClientInfo other) => id == other.id;
+        public bool Equals(ClientInfo other) => Id == other.Id;
     }
 }
