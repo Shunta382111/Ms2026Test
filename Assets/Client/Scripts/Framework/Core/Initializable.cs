cc1c2592190fcc2eea1765a9f498b761b0f33ac3
ao_nea_one <ahamayama13@gmail.com>
2025-10-23 12:38:30 +0900

    初期化のインターフェース追加

---
+++ Assets/Client/Scripts/Framework/Core/Initializable.cs
@@ -0,0 +1,30 @@
+﻿namespace Framework
+{
+    public interface IInitializable
+    {
+        public bool IsInitialized { get; }
+
+        /// <summary>
+        /// 初期化関数
+        /// </summary>
+        /// <remarks>
+        /// ※ 初期化関数内で、IsInitialized の中身を true に変更してください<br/>
+        /// 　 初期化が成功しているか、IsInitialized で判定します
+        /// </remarks>
+        public void Initialise();
+
+
+
+        /// <summary>
+        /// 初期化したことを保証します
+        /// </summary>
+        /// <remarks>
+        /// 未初期化の場合、アサートを出します
+        /// </remarks>
+        public void EnsureInitialized()
+        {
+            if (IsInitialized) return;
+            DebugEx.Assertion($"未初期化です: {GetType().Name}");
+        }
+    }
+}
